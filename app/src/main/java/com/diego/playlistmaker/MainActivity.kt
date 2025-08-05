d97b0d85fc3ecbdcc3560385e6b8013b4e7c2d45
Diego <egoroof1@gmail.com>
2025-08-05 23:02:10 +0300

    finish sprint 8

--- app/src/main/java/com/diego/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/diego/playlistmaker/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.diego.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.btn_search)
+        val btnMediaLibrary = findViewById<Button>(R.id.btn_media_library)
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+
+
+        btnSearch.setOnClickListener{
+            startActivity(Intent(this, SearchActivity::class.java))
+        }
+
+        btnMediaLibrary.setOnClickListener {
+
+            startActivity(Intent(this, MediaActivity::class.java))
+        }
+
+        btnSettings.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/diego/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.diego.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val btnBack = findViewById<TextView>(R.id.btn_back_to_main)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
