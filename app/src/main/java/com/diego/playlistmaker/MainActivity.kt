974856ea9bd500df0a50506c4d63c89955134159
Diego <egoroof1@gmail.com>
2025-08-06 23:33:41 +0300

    start sprint №9

--- app/src/main/java/com/diego/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/diego/playlistmaker/MainActivity.kt
@@ -2,7 +2,6 @@ package com.diego.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,18 +16,9 @@ class MainActivity : AppCompatActivity() {
         val btnMediaLibrary = findViewById<Button>(R.id.btn_media_library)
         val btnSettings = findViewById<Button>(R.id.btn_settings)
 
-
-        val clickForObject = object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                startActivity(Intent(this@MainActivity, SearchActivity::class.java))
-            }
+        btnSearch.setOnClickListener{
+            startActivity(Intent(this, SearchActivity::class.java))
         }
-        btnSearch.setOnClickListener(clickForObject)
-
-        /*разблочить после сдачи*/
-//        btnSearch.setOnClickListener{
-//            startActivity(Intent(this, SearchActivity::class.java))
-//        }
 
         btnMediaLibrary.setOnClickListener {
 
