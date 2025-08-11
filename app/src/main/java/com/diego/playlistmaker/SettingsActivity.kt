db15c16c21748ec090a6c5366ea2c577fa52189a
Diego <egoroof1@gmail.com>
2025-08-11 19:23:54 +0300

    start sprint №9 (небольшие доработки)

--- app/src/main/java/com/diego/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/diego/playlistmaker/SettingsActivity.kt
@@ -1,17 +1,23 @@
 package com.diego.playlistmaker
 
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-        val btnBack = findViewById<ImageView>(R.id.btn_back_to_main)
+        val btnBack = findViewById<MaterialToolbar>(R.id.toolbar)
 
         btnBack.setOnClickListener {
             finish()
