71ec2ed5691e967052ebac62b6185e5299769011
Diego <egoroof1@gmail.com>
2025-08-19 21:15:58 +0300

    sprint №9 (editText and TextWatcher 1)

--- app/src/main/java/com/diego/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/diego/playlistmaker/SearchActivity.kt
@@ -1,18 +1,31 @@
 package com.diego.playlistmaker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_search)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-        val main = findViewById<ConstraintLayout>(R.id.main)
+        val btnBack = findViewById<TextView>(R.id.btn_back)
 
-        main.setOnClickListener {
+        btnBack.setOnClickListener {
             finish()
         }
+
+
     }
 }
