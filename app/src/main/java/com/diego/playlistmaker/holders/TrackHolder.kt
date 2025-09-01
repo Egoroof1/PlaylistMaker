beb5bcd90324496938ad3839116a13dd208b2a5c
Diego <egoroof1@gmail.com>
2025-09-01 22:30:21 +0300

    sprint 10 (fulfilled all the conditions for verification)

--- app/src/main/java/com/diego/playlistmaker/holders/TrackHolder.kt
+++ app/src/main/java/com/diego/playlistmaker/holders/TrackHolder.kt
@@ -1,24 +1,39 @@
 package com.diego.playlistmaker.holders
 
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.diego.playlistmaker.R
 import com.diego.playlistmaker.models.Track
 
 class TrackHolder(item: View): RecyclerView.ViewHolder(item) {
 
-    fun bind(track: Track){
-        itemView.findViewById<TextView>(R.id.name_track).text = track.trackName
-        itemView.findViewById<TextView>(R.id.artist_name).text = track.artistName
-        itemView.findViewById<TextView>(R.id.time_track).text = track.trackTime
+    private val nameTrack: TextView = item.findViewById(R.id.name_track)
+    private val artistName: TextView = item.findViewById(R.id.artist_name)
+    private val timeTrack: TextView = item.findViewById(R.id.time_track)
+    private val image: ImageView = item.findViewById(R.id.image)
 
-        val image = itemView.findViewById<ImageView>(R.id.image)
+    fun bind(track: Track){
+        nameTrack.text = track.trackName
+        artistName.text = track.artistName
+        timeTrack.text = track.trackTime
+
         Glide.with(itemView)
             .load(track.artworkUrl100)
+            .centerCrop()
             .placeholder(R.drawable.placeholder)
+            .transform(RoundedCorners(dpToPx(2f, itemView)))
             .into(image)
+    }
+
+    private fun dpToPx(dp: Float, context: View): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics).toInt()
     }
 }
