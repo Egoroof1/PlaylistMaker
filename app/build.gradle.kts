b0a812d01bc2b37248b4399fe1c3282497817ab9
Diego <egoroof1@gmail.com>
2026-01-21 18:17:18 +0300

    Sprint 19 создали Navigation Graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
 
     implementation("com.google.android.material:material:1.9.0")
 
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
