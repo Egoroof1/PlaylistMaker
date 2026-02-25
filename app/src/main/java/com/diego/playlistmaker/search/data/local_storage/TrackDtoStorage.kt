cfd86405061a1e7e880f53ce6366450e95151c9e
Diego <egoroof1@gmail.com>
2026-02-26 02:06:33 +0300

    sprint 21: sharedPrefs оставил только под тему

--- app/src/main/java/com/diego/playlistmaker/search/data/local_storage/TrackDtoStorage.kt
+++
@@ -1,9 +0,0 @@
-package com.diego.playlistmaker.search.data.local_storage
-
-import com.diego.playlistmaker.search.data.models.TrackDto
-
-interface TrackDtoStorage {
-    fun get(): List<TrackDto>
-    fun save(trackDto: TrackDto): Boolean
-    fun clear(): Boolean
-}
