876c1af959662752a45080b81bc165ca38375ac5
Diego <egoroof1@gmail.com>
2026-07-18 14:32:35 +0300

    sprint27: test

---
+++ app/src/main/java/com/diego/playlistmaker/search/ui/fragment/test.kt
@@ -0,0 +1,11 @@
+package com.diego.playlistmaker.search.ui.fragment
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "test")
+@Composable
+fun Test(){
+    Text("Test")
+}
