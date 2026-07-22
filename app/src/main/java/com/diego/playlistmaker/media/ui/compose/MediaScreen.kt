5f0fdd5fa4a682e78df776e977de9dcdc964a1d3
Diego <egoroof1@gmail.com>
2026-07-22 20:30:41 +0300

    sprint27: Finish media 2

--- app/src/main/java/com/diego/playlistmaker/media/ui/compose/MediaScreen.kt
+++ app/src/main/java/com/diego/playlistmaker/media/ui/compose/MediaScreen.kt
@@ -1,21 +1,28 @@
 package com.diego.playlistmaker.media.ui.compose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.diego.playlistmaker.R
 import com.diego.playlistmaker.compose.MyRecyclerTracks
@@ -48,13 +55,34 @@ fun MediaScreen(
         TabRow(
             selectedTabIndex = selectedTabIndex,
             modifier = Modifier.fillMaxWidth(),
-            containerColor = Color.Transparent
+            containerColor = Color.Transparent,
+            indicator = { tabPositions ->
+                Box(
+                    modifier = Modifier
+                        .tabIndicatorOffset(tabPositions[selectedTabIndex])
+                        .fillMaxWidth()
+                        .height(2.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.7f) // 80%
+                            .height(2.dp)
+                            .align(Alignment.Center)
+                            .clip(RoundedCornerShape(2.dp))
+                            .background(colorResource(R.color.string))
+                    )
+                }
+            },
+            divider = {}
         ) {
             tabs.forEachIndexed { index, title ->
                 Tab(
                     selected = selectedTabIndex == index,
                     onClick = { selectedTabIndex = index },
-                    text = { Text(text = title) }
+                    text = { Text(
+                        text = title,
+                        color = colorResource(R.color.string)
+                    ) }
                 )
             }
         }
